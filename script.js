c1cc7de6141eb7ec4d2915be32e36a4ddbf0bf5c
emjose <emjose@gmail.com>
2021-02-09 15:14:39 -0500

    reduce date font

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ toggle.addEventListener('click', (e) => {
     const html = document.querySelector('html')
     if (html.classList.contains('dark')) {
         html.classList.remove('dark')
-        e.target.innerHTML = 'Dark mode'
+        e.target.innerHTML = 'DARK MODE'
     } else {
         html.classList.add('dark')
-        e.target.innerHTML = 'Light mode'
+        e.target.innerHTML = 'LIGHT MODE'
     }
 })
 
